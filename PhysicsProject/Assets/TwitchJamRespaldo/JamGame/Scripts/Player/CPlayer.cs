5af837739d1fccafe7ee08f7065587da98eec3a8
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2023-04-30 16:23:24 -0300

    Files Pulush to work

---
+++ PhysicsProject/Assets/TwitchJamRespaldo/JamGame/Scripts/Player/CPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPlayer 
+{
+    public float mouseSensivility = 100;
+
+
+    //void Start()
+    //{
+        
+
+    //}
+
+
+}
