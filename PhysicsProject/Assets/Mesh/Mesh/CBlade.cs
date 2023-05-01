6c999d8fe2bcfac86547b2bab8829af9ffd39231
ZimmZimmGames <alexzimmer.zimmer1@gmail.com>
2023-05-01 01:45:18 -0300

    Manipulation Triangle Mesh With Transform meshs

--- PhysicsProject/Assets/Mesh/Mesh/CBlade.cs
+++ PhysicsProject/Assets/Mesh/Mesh/CBlade.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class CBlade : MonoBehaviour
 {
+
+    private const int NUM_VERTICES = 12;
     [SerializeField]
     [Tooltip("The blade object")]
     private GameObject _blade = null;
@@ -15,6 +17,8 @@ public class CBlade : MonoBehaviour
     [SerializeField]
     private GameObject _base = null;
 
+
+
     [SerializeField]
     private GameObject _meshParent = null;
 
@@ -23,13 +27,18 @@ public class CBlade : MonoBehaviour
 
     public Mesh _mesh;
 
+    [SerializeField]
+    [Tooltip("The number of frame that the trail should be rendered for")]
+    private int _trailFrameLength = 3;
+
     private Vector3[] _vertices;
     private int[] _triangles;
     private int _frameCount;
-
-    private Vector3 _PreviousTipPosition;
-    private Vector3 _previousBasPosition;
-    private Vector3 _TriangleEnterTiPosition;
+    
+    private Vector3 _previousTipPosition;
+    private Vector3 _previousBasePosition;
+    private Vector3 _triggerEnterTipPosition;
+    private Vector3 _triggerEnterBasePosition;
     private Vector3 _triggerExitTipPosition;
 
     public int PolySides;
@@ -39,32 +48,60 @@ public class CBlade : MonoBehaviour
     
      void Start()
     {
-       // _meshParent.transform.position = Vector3.zero;
+        _meshParent.transform.position = Vector3.zero;
         _mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = _mesh;
         //_meshParent.GetComponent<MeshFilter>().mesh = _mesh;
-        CreateShape();
-        UpdateMesh();
+        //_vertices = new Vector3[_t]
+        _vertices = new Vector3[_trailFrameLength * NUM_VERTICES];
+        _triangles = new int[_vertices.Length];
+
+        _previousTipPosition = _tip.transform.position;
+      _previousBasePosition = _base.transform.position;
+       
+        //
         //_meshParent.transform.position = Vector3.zero;
         //_mesh = new Mesh();
         //_meshParent.GetComponent<MeshFilter>().mesh = _mesh;
     }
-
-    void CreateShape ()
+    private void LateUpdate()
     {
-        _vertices = new Vector3[]
+        CreateShape();
+        UpdateMesh();
+    }
+    void CreateShape()
+    {
+        if (_frameCount == (_trailFrameLength * NUM_VERTICES))
         {
-            new Vector3(0,0,0),
-            new Vector3(0,0,1),
-            new Vector3(1,0,0),
-            new Vector3 (1,0,1),
-        };
-        _triangles = new int[]
-        {
-            0, 1, 2,
-            1, 3, 2
-        };
-
+            _frameCount = 0;
+        }
+        //_vertices[_frameCount] = _base.transform.position;
+        //_vertices[_frameCount + 1] = _tip.transform.position;
+        //_vertices[_frameCount + 2] = _previousTipPosition;
+        //_vertices[_frameCount + 3] = _base.transform.position;
+        //_vertices[_frameCount + 4] = _previousTipPosition;
+        //_vertices[_frameCount + 5] = _tip.transform.position;
+        _vertices[_frameCount] = _base.transform.position;
+        _vertices[_frameCount + 1] = _tip.transform.position;
+        _vertices[_frameCount + 2] = _blade.transform.position;
+        _vertices[_frameCount + 3] = _base.transform.position;
+        _vertices[_frameCount + 4] = _blade.transform.position;
+        _vertices[_frameCount + 5] = _tip.transform.position;
+        //_vertices = new Vector3[]
+        //{
+        _triangles[_frameCount] = _frameCount;
+        _triangles[_frameCount + 1] = _frameCount + 1;
+        _triangles[_frameCount + 2] = _frameCount + 2;
+        _triangles[_frameCount + 3] = _frameCount + 3;
+        _triangles[_frameCount + 4] = _frameCount + 4;
+        _triangles[_frameCount + 5] = _frameCount + 5;
+        //};
+        //_triangles = new int[]
+        //{
+        //    0,1,2,
+        //    1,3,2
+        //};
+        _frameCount += NUM_VERTICES;
     }
 
      void UpdateMesh()
